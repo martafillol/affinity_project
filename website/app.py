91227627e854ee2fc05c99942c83ca1fcf87dc64
pablo lizardi <pabloagra3018@gmail.com>
2024-06-07 15:44:46 +0200

    website

--- website/app.py
+++ website/app.py
@@ -5,7 +5,45 @@ import random
 from openai import OpenAI
 import os
 
+st.set_page_config(
+    page_title="Affinity Adv",  # => Quick reference - Streamlit
+    page_icon="🪲",
+    layout="centered",  # wide
+)
+CSS = """
+
+
+body {
+  font-family: Overpass;
+  background-color: #f9ebc88d;
+  color: #644536;
+  text-align: center;
+  padding: 25px;
+}
+
+h1, h2, h3 {
+  font-family: Roboto;
+  color: #0C0A3E;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+  background-color: #BA6E6E;
+  padding: 15px;
+  border-radius: 5px;
+  transition: 0.3s ease;
+}
+
+a:hover {
+  background-color: #A36060;
+}
+"""
+
+st.write(f"<style>{CSS}</style>", unsafe_allow_html=True)
+
 client = OpenAI()
+
 
 def generate_prompt(topic, user_age, gender, city, interest):
     return f'''create an image based on this topic: {topic}
@@ -14,9 +52,9 @@ def generate_prompt(topic, user_age, gender, city, interest):
                that lives in the city of: {city}. That is interested in {interest}.'''
 
 # Generate a random user context
-topic = "a selection of topics"
+topic = ['sports','fashion','gaming','movies'][random.randint(0, 3)]
 user_age = random.randint(18, 80)
-genders = ['male', 'female']
+genders = ['male', 'female'][random.randint(0, 1)]
 cities = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Philadelphia', 'Phoenix', 'San Antonio', 'San Diego', 'Dallas']
 interest = 'User interest'
 gender = random.choice(genders)
@@ -34,7 +72,6 @@ response = client.images.generate(
     size="1024x1024"
 )
 
-st.set_page_config(initial_sidebar_state="collapsed")
 
 def main_page():
     st.title("Title")
@@ -42,6 +79,7 @@ def main_page():
 
     # Create a form with a submit button
     with st.form(key='my_form'):
+
         st.write("Fill in the form and press submit to go to the Creative Demo page")
         user_input = st.text_input(label="Enter text here", max_chars=200)
         submit_button = st.form_submit_button(label='Submit')
