b43d22d1e3bc8ed7daaf535c10e973fc9cf637af
pablo lizardi <pabloagra3018@gmail.com>
2024-06-11 11:11:10 +0200

    Functional version of the st page

--- website/app.py
+++ website/app.py
@@ -2,8 +2,14 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import random
+import requests
+from bs4 import BeautifulSoup
 from openai import OpenAI
 import os
+import spacy
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 st.set_page_config(
     page_title="Affinity Adv",  # => Quick reference - Streamlit
@@ -46,40 +52,12 @@ a:hover {
 
 st.write(f"<style>{CSS}</style>", unsafe_allow_html=True)
 
-client = OpenAI()
-best_fit_interest = ['sports','movies','children','party','gaming'][random.randint(0, 4)]
-avg_age_of_cluster = random.randint(18, 80)
-top_5_other_interests = ['fashion', 'gaming', 'movies'][random.randint(0, 2)]
-
-def generate_prompt(best_fit_interest, avg_age_of_cluster, top_5_other_interests):
-    return f'''create a realistic advertisement image based on this topic: {best_fit_interest}
-               for a user of age: {int(avg_age_of_cluster)}
-               with top 5 other interests: {top_5_other_interests}.'''
-
-
-# Generate a random user context
-# topic = ['sports','fashion','gaming','movies'][random.randint(0, 3)]
-# user_age = random.randint(18, 80)
-# genders = ['male', 'female'][random.randint(0, 1)]
-# cities = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Philadelphia', 'Phoenix', 'San Antonio', 'San Diego', 'Dallas']
-# interest = 'User interest'
-# gender = random.choice(genders)
-# city = random.choice(cities)
 
 # Initialize session state
 if 'page' not in st.session_state:
     st.session_state.page = 'main'
 
-prompt = generate_prompt(best_fit_interest, avg_age_of_cluster, top_5_other_interests)
 
-response = client.images.generate(
-    model="dall-e-3",
-    prompt=prompt,
-    size="1024x1024"
-)
-
-
-def main_page():
     st.title("Affinity")
     st.write("Predicting the right Ad")
 
@@ -88,15 +66,31 @@ def main_page():
 
         st.write("Fill in the form and press submit to go to the Creative Demo page")
         user_input = st.text_input(label="Enter text here", max_chars=200)
+        url = user_input
+        response = requests.get("https://affinity-dzgegmrtba-no.a.run.app/process-urls", params={"url_input":url})
+        st.write(response.json())
         submit_button = st.form_submit_button(label='Submit')
+        if submit_button:
+
+            client = OpenAI()
+            best_fit_interest = ['sports','movies','children','party','gaming'][random.randint(0, 4)]
+            avg_age_of_cluster = random.randint(18, 80)
+            top_5_other_interests = ['fashion', 'gaming', 'movies'][random.randint(0, 2)]
+            prompt =    f'''create a realistic advertisement image based on this topic: {best_fit_interest}
+                        for a user of age: {int(avg_age_of_cluster)}
+                        with top 5 other interests: {top_5_other_interests}.'''
+            response = client.images.generate(
+            model="dall-e-3",
+            prompt=prompt,
+            size="1024x1024"
+            )
+            st.session_state.page = 'creative_demo'
+            st.session_state['user_input'] = user_input
+            st.session_state['prompt'] = prompt
+            st.session_state['response'] = response
 
     # When the submit button is pressed, set session state to show the creative demo page
-    if submit_button:
-        st.session_state.page = 'creative_demo'
-        st.session_state['user_input'] = user_input
-        st.session_state['prompt'] = prompt
-        st.session_state['response'] = response
-        st.experimental_rerun()
+
 
 # Display the appropriate page based on the session state
 if st.session_state.page == 'main':
@@ -110,3 +104,5 @@ if st.session_state.page == 'creative_demo':
     if st.button("Go Back to Main Page", key='back_to_main_app'):
         st.session_state.page = 'main'
         st.experimental_rerun()
+
+#
