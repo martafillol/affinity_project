95e45dfa8dff9a24287d14ce0d2ee07801517e42
pablo lizardi <pabloagra3018@gmail.com>
2024-06-13 11:16:22 +0200

    pack and req

--- website/app.py
+++ website/app.py
@@ -225,10 +225,16 @@ if button:
         banner_path = banner_path[1]
     elif cluster == 2:
         banner_path = banner_path[2]
+    elif cluster == 3:
+        banner_path = banner_path[2]
+    elif cluster == 4:
+        banner_path = banner_path[2]
+    elif cluster == 5:
+        banner_path = banner_path[2]
 
     st.write(banner_path)
 
-    injected = inject_banner(url, banner_path[0], output_file, width, height)
+    injected = inject_banner(url, banner_path, output_file, width, height)
 
     if injected:
         st.image(output_file, caption="MMA Banner Screenshot")
